57b7d601b97effe60345b1447105cca5060713dc
root <root@Emil-Karapetyan.localdomain>
2024-12-09 16:14:12 +0400

    Fix for complaints

--- matrix.h
+++ matrix.h
@@ -2,28 +2,33 @@
 #include<iostream>
 #include<exception>
 #include<memory>
-template<typename T>
-class Matrix {
+
+template<typename T, typename = typename
+	 std::enable_if_t<std::is_integral<T>::value>>
+class Matrix final {
 
 	public:
-		Matrix() :  m_width{0}, m_height{0}, m_matrix{nullptr} {};
-		Matrix(size_t width, size_t height, T val=0) : m_width{width}, m_height{height} {
-			m_matrix.resize(m_height);
+		Matrix() :  m_width{0}, m_height{0} {};
+		Matrix(const size_t width, const size_t height, const T val=0) 
+			: m_width{width}, m_height{height}, m_data(m_height) {
+			m_data.resize(m_height);
 			for (size_t hIdx = 0; hIdx < m_height; ++hIdx) {
-				m_matrix[hIdx] = std::make_shared<std::vector<T>>(m_width);
+				m_data[hIdx] = std::vector<T>(m_width);
 			}
 
 			for (size_t hIdx = 0; hIdx < m_height; ++hIdx) {
 				for (size_t wIdx = 0; wIdx <m_width ; ++wIdx) {
-					m_matrix[hIdx]->at(wIdx) = val;
+					m_data[hIdx][wIdx] = val;
 				}
 			}
 		}
 
-		Matrix(const Matrix& matr) : Matrix(matr.getWidth(), matr.getHeight()) {
+		Matrix(const Matrix& matr)
+			: m_width{matr.getWidth()}, m_height{matr.getHeight()}, m_data{m_height} {
 			for (size_t hIdx = 0; hIdx < m_height; ++hIdx) {
+				m_data[hIdx] = std::vector<T>(m_width);
 				for (size_t wIdx = 0; wIdx <m_width ; ++wIdx) {
-					m_matrix[hIdx]->at(wIdx) = matr.getCellValue(hIdx, wIdx);
+					m_data[hIdx][wIdx] = matr[hIdx][wIdx];
 				}
 			}
 		}
@@ -32,9 +37,9 @@ class Matrix {
 			m_height = matr.getHeight();
 			m_width = matr.getWidth();
 
-			m_matrix.resize(m_height);
+			m_data.resize(m_height);
 			for (size_t hIdx = 0; hIdx < m_height; ++hIdx) {
-				m_matrix[hIdx] = matr[hIdx];
+				m_data[hIdx] = matr[hIdx];
 			}
 		}
 		
@@ -44,28 +49,24 @@ class Matrix {
 					m_height = matr.getHeight();
 					m_width = matr.getWidth();
 
-					m_matrix.resize(m_height);
+					m_data.resize(m_height);
 					for (size_t hIdx = 0; hIdx < m_height; ++hIdx) {
-						m_matrix[hIdx] = new std::vector<T>[m_width];
+						m_data[hIdx] = new std::vector<T>[m_width];
 					}
 				}
 				for (size_t hIdx = 0; hIdx < m_height; ++hIdx) {
 					for (size_t wIdx = 0; wIdx <m_width ; ++wIdx) {
-						m_matrix[hIdx]->at(wIdx) = matr.getCellValue(hIdx, wIdx);
+						m_data[hIdx][wIdx] = matr[hIdx][wIdx];
 					}
 				}
 			}
 			return *this;
 		}
 
-		~Matrix() {
-		//	delete[] m_matrix;
+		~Matrix() = default{
+		//	delete[] m_data;
 		}
 		
-		const T& getCellValue(size_t height, size_t width) const {
-			return m_matrix[height]->at(width);
-		}
-
 		inline const size_t getWidth() const {
 			return m_width;
 		}
@@ -74,27 +75,29 @@ class Matrix {
 			return m_height;
 		}
 
-		std::shared_ptr<std::vector<T>> operator[](size_t rowIdx) const {
-			return m_matrix[rowIdx];
+		std::vector<T> operator[](size_t rowIdx) const {
+			return m_data[rowIdx];
 		}
 		
-		inline  std::vector<std::vector<T>*> getMatrix() const {
-			return m_matrix;
+		inline const std::vector<std::vector<T>*>& getData() const {
+			return m_data;
 		}
-		inline void setWidth(size_t width) {
+		inline void setWidth(const size_t width) {
 			m_width = width;
 		}
 
-		inline void setHeight(size_t height) {
+		inline void setHeight(const size_t height) {
 			m_height = height;
 		}
 
 		void print() {
-			if (m_matrix.empty())
+			if (m_data.empty()) {
 				return;
+			}
+
 			for (size_t hIdx = 0; hIdx < m_height; ++hIdx) {
 				for (size_t wIdx = 0; wIdx <m_width ; ++wIdx) {
-					std::cout<< m_matrix[hIdx]->at(wIdx) << " ";
+					std::cout<< m_data[hIdx][wIdx] << " ";
 				}
 				std::cout<<"\n";
 			}
@@ -103,5 +106,5 @@ class Matrix {
 	private:
 		size_t m_width;
 		size_t m_height;
-		std::vector<std::shared_ptr<std::vector<T>>> m_matrix;
+		std::vector<std::vector<T>> m_data;
 };
