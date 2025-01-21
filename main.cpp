1ded6694a0664f85eb67107c4ba8e8e853e3eae7
EmilKarapetyan <karapetyanemil1@gmail.com>
2025-01-21 17:52:33 +0400

    Added several checks as well as some functionality

--- main.cpp
+++ main.cpp
@@ -1,84 +1,184 @@
-#include<iostream>
+// Includes from this project
+
+// Includes from third-party projects (like Boost, Poco, etc.)
+
+// Includes from C++ Standard Library
+
 #include"matrix.h"
+
+#include<iostream>
+
 #include<random>
 
 //#include <googletest/gtest.h>
 
+bool test_copy_assignment_operator()
+{
+	Matrix<std::int32_t> mtx(4, 4, 1);
+	Matrix<std::int32_t> mat_second;
+	mat_second = mtx;
 
-int fib(int n) {
-	if (n <= 1)
-		return n;
-	return fib(n-1) + fib(n-2);
+	bool failed = false;
+	for(std::uint32_t i = 0; i < 4; ++i) {
+		for(std::uint32_t j = 0; j < 4; ++j) {
+			if(mtx[i][j] != mat_second[i][j]) {
+				std::cout << "TEST FAILED: mtx assignment operator"<<std::endl;
+				return failed;
+			}
+		}
+	} 
+	std::cout << "TEST SUCCEED: mtx assigmnet operaotr"<<std::endl;
+	return true;
 }
 
+bool test_move_assignment_operator() 
+{
+	Matrix<std::int32_t> matr_orig(4, 4, 1);
+	Matrix<std::int32_t> mtx_copy;
+	mtx_copy = matr_orig;
+	Matrix<std::int32_t> mtx;
+	mtx = std::move(matr_orig);
+
+	bool failed = false;
+	for(std::uint32_t i = 0; i < 4; ++i) {
+		for(std::uint32_t j = 0; j < 4; ++j) {
+			if(mtx_copy[i][j] != mtx[i][j]) {
+				std::cout << "TEST FAILED: mtx move assignment operator"<<std::endl;
+				return failed;
+			}
+		}
+	} 
+	std::cout << "TEST SUCCEED: mtx move assigmnet operaotr"<<std::endl;
+	return true;
+}
+
+bool test_matrix_scalar_multiplication()
+{
+	const std::int32_t multiplier = 4;
+	Matrix<std::int32_t> matr1(6,6,5);
+	Matrix<std::int32_t> matr2 = matr1;
+	matr2 *= multiplier;
+
+	std::uint32_t mtxrow = matr1.getRow();
+	std::uint32_t mtxcol = matr1.getColumn();
+	bool failed = false;
+	if (mtxrow != matr2.getRow() || mtxcol != matr2.getColumn()) {
+		std::cout << "TEST FAILED: mtx scalar multiplication"<<std::endl;
+		return failed;
+	}
+
+	for(std::uint32_t i = 0; i < mtxrow; ++i) {
+		for(std::uint32_t j = 0; j < mtxcol; ++j) {
+			if((matr1[i][j]) * multiplier != matr2[i][j]) {
+				std::cout << "TEST FAILED: mtx multiplication"<<std::endl;
+				return failed;
+			}
+		}
+	} 
+	std::cout << "TEST SUCCEED: mtx scalar multiplication"<<std::endl;
+	return true;
+}
+
+bool test_matrix_product()
+{
+	Matrix<std::int32_t> res;
+	Matrix<std::int32_t> matr1(3, 3,
+		{{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9}});
+
+	Matrix<std::int32_t> matr2(3, 7,
+		{{1, 2, 3, 4, 5, 6, 7},
+		{8, 9, 10, 11, 12, 13, 14},
+		{15, 16, 17, 18, 19, 20, 21}});
+
+	Matrix<std::int32_t> product_golden_1(3, 7,
+		{{62, 68, 74, 80, 86, 92, 98},
+		{134, 149, 164, 179, 194, 209, 224},
+		{206, 230, 254, 278, 302, 326, 350}});
+
+	matr1.matrixProduct(matr2, res);
+
+	if (res != product_golden_1) 
+	{
+		std::cout << "TEST FAILED: mtx product"<<std::endl;
+		return false;
+	}
+
+	Matrix<std::int32_t> matr3(6, 5,
+		{{1, 2, 3, 4, 5},
+        {6, 7, 8, 9, 10},
+        {11, 12, 13, 14, 15},
+        {16, 17, 18, 19, 20},
+        {21, 22, 23, 24, 25},
+        {26, 27, 28, 29, 30}});
+
+	Matrix<std::int32_t> matr4(5, 8,
+		{{1, 2, 3, 4, 5, 6, 7, 8},
+        {9, 10, 11, 12, 13, 14, 15, 16},
+        {17, 18, 19, 20, 21, 22, 23, 24},
+        {25, 26, 27, 28, 29, 30, 31, 32},
+        {33, 34, 35, 36, 37, 38, 39, 40}});
+
+	Matrix<std::int32_t> product_golden_2(6, 8,
+		{{335, 350, 365, 380, 395, 410, 425, 440},
+        {760, 800, 840, 880, 920, 960, 1000, 1040},
+        {1185, 1250, 1315, 1380, 1445, 1510, 1575, 1640},
+        {1610, 1700, 1790, 1880, 1970, 2060, 2150, 2240},
+        {2035, 2150, 2265, 2380, 2495, 2610, 2725, 2840},
+        {2460, 2600, 2740, 2880, 3020, 3160, 3300, 3440}});
+
+
+	matr3.matrixProduct(matr4, res);
+	if (res != product_golden_2) 
+	{
+		std::cout << "TEST FAILED: mtx product"<<std::endl;
+		return false;
+	}	
+	
+	std::cout << "TEST SUCCEED: mtx product"<<std::endl;
+	return true;
+}
+
+bool test_determinant_calculation()
+{
+	Matrix<int> matr1(8, 8,
+		{{1045, 2201, 3204, 1492, 4310, 2823, 5172, 9985},
+        {5732, 1938, 3014, 4102, 2111, 5074, 2357, 1753},
+        {1212, 2013, 4010, 5383, 3022, 4832, 5245, 6111},
+        {7310, 3820, 2276, 5992, 3404, 5599, 2035, 4927},
+        {5156, 4281, 7321, 1837, 6740, 5493, 1832, 4602},
+        {2842, 1021, 5067, 1049, 5317, 5273, 6130, 2536},
+        {1933, 7230, 3829, 2976, 1892, 3278, 6032, 4111},
+        {5194, 6723, 4555, 6148, 3960, 7282, 1549, 4389}});
+
+	Matrix<int> matr2(4, 4,
+		{{71, 60, 42, 80},
+		{12, 90, 59, 12},
+		{19, 97, 63, 82},
+		{40, 27, 36, 9}});
+		
+	//std::cout<<"\nDeterminant of "<<matr2.get_determinant()<<std::endl;; //TODO Revisit
+	return true;
+}
+
+bool test_functionality()
+{
+	return  (test_copy_assignment_operator() &
+			test_move_assignment_operator() &
+			test_matrix_scalar_multiplication() &
+			test_matrix_product());
+}
 
 int main() {
-	const size_t size = 4;
-	Matrix<int> matr(size, size); //default constructor
-	matr.fillMatrix();
 
-	matr.print();
-	matr = matr.divide(2,3);
-	std::cout<<"Minor of matrix \n";
-	matr.print();
-
-	Matrix<int> matr1(4,4);
-	Matrix<int> matr2(4,4,6); //parametrised constructor
-
-	std::cout<<"Printing matrix 1\n";
-	matr1.print();
-	std::cout<<"Printing matrix 2\n";
-	matr2.print();
-	Matrix<int> matr3(matr2); //copy constructor
-
-	std::cout<<"Printing matrix 3\n";
-	matr3.print();
-
-	Matrix<int> matr4 = matr3; //copy assignment
-	std::cout<<"Printing matrix 4\n";
-	matr4.print();
-	
-	Matrix<int> matr12(4,4, 5);
-	Matrix<int> matr5 = std::move(matr12); // move constructor
-	
-	std::cout<<"Printing matrix before move assignemnt\n";
-	matr5.print();
-	
-	//matr5 = std::move(matr4); //move assignemnt
-
-	std::cout<<"Printing matrix after move assignemnt\n";
-	matr5.print();
-	//matr4.print();
-
-	std::cout<<"Printing matrix before Multiplication\n";
-	matr5.print();
-	std::cout<<"Printing matrix after Multiplication\n";
-	matr5 *= 2;
-	matr5.print();
-
-	try {
-	//	matr5.at(6, 2);
+	bool status = test_functionality();
+	if (status) 
+	{
+		std::cout<<"####################"<<std::endl;
+		std::cout<<"ALL THE TEST PASSED! Ibrahim ջան"<<std::endl;
+		std::cout<<"####################"<<std::endl;
 	}
-	catch(std::exception& ex) {
-		std::cout<<ex.what()<<"\n";
-	}
-
-	Matrix<int> matr6(4,4);
-	matr6.fillMatrix();
-	
-	std::cout<<"Print matr Before Transposal \n";
-	matr6.print();
-	std::cout<<"\n############################################\n";
-	matr6.transpose();
-	matr6.print();
-
-	int determinant = matr6.determinantOfMatrix(matr6.getHeight());
-	std::cout<<"determinant of matr6 is "<< determinant << "\n";
-
-	Matrix<int> res;
-	matr5.mulMatrix(matr3, res);
-	std::cout<<"\n############################################\n";
-	res.print();
 
 	return 0;
 }
--- matrix.h
+++ matrix.h
@@ -1,6 +1,8 @@
 #include <vector>
 #include <iostream>
 #include <exception>
+#include <algorithm>
+
 #include <random>
 #include <time.h>
 
@@ -8,87 +10,136 @@ template<typename T, typename = typename
 	 std::enable_if_t<std::is_integral<T>::value>>
 class Matrix final {
 	public:
-		Matrix() :  m_height{0}, m_width{0} = default;
+		Matrix() :  m_row{0}, m_col{0} {};
 
-		Matrix(const std::size_t height, const size_t width, constexpr T val=0) 
-			: m_height{height}, m_width{width}, m_data(m_height) {
-			for (size_t hIdx = 0; hIdx < m_height; ++hIdx) {
-				m_data[hIdx].resize(m_width, val);
+		Matrix(const std::size_t row, const std::size_t col, const T val=0) 
+			: m_row{row}, m_col{col}, m_data(m_row) {
+			for (std::size_t hIdx = 0; hIdx < m_row; ++hIdx) {
+				m_data[hIdx].resize(m_col, val);
+			}
+		}
+		
+		Matrix(const std::size_t row, const size_t col,
+			   const std::initializer_list<std::initializer_list<int>>& initList)
+			   : m_row{row}, m_col{col} 
+		{
+
+			if (initList.size() != row) {
+				throw std::invalid_argument("Number of rows in initializer_list does not match row.");
+			}
+			for (const auto& row : initList) {
+				m_data.emplace_back(row.begin(), row.end());
 			}
 		}
 
-		Matrix(const Matrix& matr) : m_height{matr.getHeight()}, m_width{matr.getWidth()}, m_data{matr.m_data} {}
+		Matrix(const Matrix& matr) : m_row{matr.getRow()}, m_col{matr.getColumn()}, m_data{matr.m_data} {}
 
 		Matrix(Matrix&& matr) 
-			: m_width(std::move(matr.getWidth())),
-			  m_height(std::move(matr.getHeight())),
+			: m_col(std::move(matr.getColumn())),
+			  m_row(std::move(matr.getRow())),
 			  m_data(std::move(matr.m_data)) {}
 		
 		
-		Matrix& operator=(Matrix<T>&& matr) {
-			m_width = std::move(matr.m_width);
-			m_height = std::move(matr.m_height);
+		Matrix& operator=(Matrix<T>&& matr) 
+		{
+			m_col = std::move(matr.m_col);
+			m_row = std::move(matr.m_row);
 			m_data = std::move(matr.m_data);
 			return *this;
 		}
 
-		Matrix& operator=(const Matrix& matr) {
-			m_width = matr.getWidth();
-			m_height = matr.getHeight();
+		Matrix& operator=(const Matrix& matr)
+		{
+			m_col = matr.getColumn();
+			m_row = matr.getRow();
 			m_data.assign(matr.m_data.begin(), matr.m_data.end());				
 			return *this;
 		}
 		
-		inline const size_t getWidth() const {
-			return m_width;
-		}
-		inline const size_t getHeight() const {
-			return m_height;
-		}
-		inline void setWidth(const size_t width) {
-			m_width = width;
-		}
-		inline void setHeight(const size_t height) {
-			m_height = height;
-		}
-		inline const std::vector<std::vector<T>*>& getData() const {
-			return m_data;
-		}
-		inline void setValue(const size_t height, const size_t width, const T val) {
-			m_data.at(height).at(width) = val;
-		}
-		inline T getValue(const size_t height, const size_t width) const {
-			return m_data[height][width];
+		bool operator==(const Matrix& matr) const
+		{
+			if (matr.getRow() != m_row || matr.getColumn() != m_col)
+			{
+				return false;
+			}
+
+			for (int hIdx = 0; hIdx < m_row; ++hIdx) {
+				for (int wIdx = 0; wIdx < m_col; ++wIdx) {
+					if (m_data[hIdx][wIdx] != matr[hIdx][wIdx])
+					{
+						return false;
+					}
+				}
+			}
+			return true;
 		}
 
-		T at(const size_t height, const size_t width) {
-			return m_data.at(height).at(width);
+		bool operator!=(const Matrix<T>& matr) const 
+		{
+        	return !(*this == matr);
+    	}
+
+		inline const size_t getColumn() const
+		{
+			return m_col;
 		}
-		std::vector<T> operator[](const size_t rowIdx) const {
-			return m_data[rowIdx];
+		inline const size_t getRow() const 
+		{
+			return m_row;
+		}
+		inline void setcol(const size_t col) 
+		{
+			m_col = col;
+		}
+		inline void setrow(const size_t row) 
+		{
+			m_row = row;
+		}
+		inline const std::vector<std::vector<T>*>& getData() const 
+		{
+			return m_data;
+		}
+		inline void setValue(const size_t row, const size_t col, const T val) 
+		{
+			m_data.at(row).at(col) = val;
+		}
+		inline T getValue(const size_t row, const size_t col) const 
+		{
+			return m_data[row][col];
+		}
+		T at(const size_t row, const size_t col) 
+		{
+			return m_data.at(row).at(col);
+		}
+		std::vector<T>& operator[](const size_t rowIdx)  
+		{
+			return m_data.at(rowIdx);
+		}
+		std::vector<T> operator[](const size_t rowIdx) const 
+		{
+			return m_data.at(rowIdx);
 		}
 		void operator*=(T val) {
-			for (size_t hIdx = 0; hIdx < m_height; ++hIdx) {
-				for (size_t wIdx = 0; wIdx < m_width ; ++wIdx) {
+			for (size_t hIdx = 0; hIdx < m_row; ++hIdx) {
+				for (size_t wIdx = 0; wIdx < m_col ; ++wIdx) {
 					m_data[hIdx][wIdx] *= val;
 				}
 			}
 		}
 		
-		Matrix<T> slice(const size_t i, const size_t j) {
-			if (i < m_height && j < m_width) {
-				Matrix<T> minor(m_height-1, m_width-1);
-				int row = 0, 
+		Matrix<T> get_submatrix(const size_t row, const size_t col) 
+		{
+			if (row < m_row && col < m_col) {
+				Matrix<T> minor(m_row-1, m_col-1);
+				int row = 0;
 				int col = 0;
-				for (int hIdx = 0; hIdx < m_height; ++hIdx) {
-					if (hIdx == i)
-					{
+				for (std::uint32_t hIdx = 0; hIdx < m_row; ++hIdx) {
+					col = 0;
+					if (hIdx == row) {
 						continue;
 					}
-					col = 0;
-					for (int wIdx = 0; wIdx < m_width; ++wIdx) {
-						if (wIdx == j)
-						{
+					for (std::uint32_t wIdx = 0; wIdx < m_col; ++wIdx) {
+						if (wIdx == col) {
 							continue;
 						}
 						minor.setValue(row, col++, m_data[hIdx][wIdx]);
@@ -100,11 +151,12 @@ class Matrix final {
 			return *this;
 		}
 
-		void getCofactorMatrix(Matrix<T>& tem, const size_t p1, const size_t q1, const size_t num) {  
-			int i = 0, 
-			int j = 0;   
-			for (int rows = 0; rows < num; ++rows) {
-				for (int cols = 0; cols < num; ++cols) {    
+		void getCofactorMatrix(Matrix<T>& tem, const size_t p1, const size_t q1, const size_t num) 
+		{  
+			std::uint32_t i = 0;
+			std::uint32_t j = 0;
+			for (std::uint32_t rows = 0; rows < num; ++rows) {
+				for (std::uint32_t cols = 0; cols < num; ++cols) {    
 					if (rows != p1 && cols != q1) {  
 						tem.setValue(i, j++, m_data[rows][cols]);
 						if (j == num - 1) {  
@@ -116,71 +168,98 @@ class Matrix final {
 			}  
 		}  
 		
-		int determinantOfMatrix(const size_t matrixSize) {  
-			int num = matrixSize;
-			int determinant = 0;
-			if (num == 1) {
-				return m_data[0][0];
-			}
+		std::int32_t get_determinant() {
+        if (m_row != m_col) {
+            throw std::invalid_argument("Matrix must be square to calculate determinant.");
+        }
 
-			Matrix<T> tem(matrixSize-1, matrixSize-1);
-			int signs = 1;
-			for (int i = 0; i < num; ++i) {
-				getCofactorMatrix(tem, 0, i, num);
-				determinant += signs * m_data[0][i] * tem.determinantOfMatrix(num - 1);
-				signs = -signs;
-			}  
+        Matrix<int> matrix(*this);
+        T det = 1;
+
+        for (std::size_t i = 0; i < m_row; ++i) {
+            std::size_t max_row = i;
+            for (std::size_t j = i + 1; j < m_row; ++j) {
+            if (std::abs(matrix[j][i]) > std::abs(matrix[max_row][i])) {
+                    max_row = j;
+                }
+            }
+
+            if (matrix[max_row][i] == 0) 
+			{
+                return 0;
+            }
+
+            if (max_row != i) 
+			{
+                std::swap(matrix[max_row], matrix[i]);
+                det *= -1;
+            }
+
+            for (std::size_t j = i + 1; j < m_row; ++j) {
+                double factor = matrix[j][i] / matrix[i][i];
+                for (size_t k = i; k < m_row; ++k) {
+                    matrix[j][k] -= factor * matrix[i][k];
+                }
+            }
+        }
+
+        // Multiply diagonal elements to get the determinant
+        for (std::size_t i = 0; i < m_row; ++i) {
+            det *= matrix[i][i];
+        }
+
+        return det;
+    }
 		
-			return determinant;
-		}
-		
-		Matrix<T>& mulMatrix(const Matrix<T>& mat1, const Matrix<T>& mat2) const {
-			Matrix<T> res(mat1.m_width, mat2.m_height);
+		void matrixProduct(const Matrix<T>& mat2, Matrix<T>& result) const 
+		{
+			Matrix<T> res(m_row, mat2.getColumn());
 
-			const int row1 = m_height;
-			const int col1 = m_width;
-			const int row2 = mat2.getHeight();
-
+			const std::uint32_t row1 = m_row;
+			const std::uint32_t col1 = m_col;
+			const std::uint32_t row2 = mat2.getRow();
 			if (col1 != row2) {
 				throw std::out_of_range("Invalid Input");
 			}
 
-			const int col2 = mat2[0].size();
-		
-			for (int i = 0; i < row1; i++) {
-				for (int j = 0; j < col2; j++) {
-					for (int k = 0; k < col1; k++) {
-						res.setValue(i, j, resultingMatrix[i][j] + (m_data[i][k] * mat2[k][j]));
+			const std::uint32_t col2 = mat2[0].size();
+			for (std::uint32_t i = 0; i < row1; i++) {
+				for (std::uint32_t j = 0; j < col2; j++) {
+					for (std::uint32_t k = 0; k < col1; k++) {
+						res.setValue(i, j, res[i][j] + (m_data[i][k] * mat2[k][j]));
 					}
 				}
 			}
-			return resultingMatrix;
+			result = std::move(res);
 		}
 
-		void transpose() {
-			for (int i = 0; i < m_height; ++i ) {
-				for (int j = i+1; j < m_height; ++j) {
+		void transpose() 
+		{
+			for (std::uint32_t i = 0; i < m_row; ++i ) {
+				for (std::uint32_t j = i+1; j < m_row; ++j) {
 					std::swap(m_data[i][j], m_data[j][i]);
 				}
 				
 			}
 		}
 
-		void print() {
-			for (std::size_t hIdx = 0; hIdx < m_height; ++hIdx) {
-				for (size_t e_t wIdx = 0; wIdx < m_width; ++wIdx) {
+		void print()
+		{
+			for (std::uint32_t hIdx = 0; hIdx < m_row; ++hIdx) {
+				for (std::uint32_t wIdx = 0; wIdx < m_col; ++wIdx) {
 					std::cout << m_data[hIdx][wIdx] << " ";
 				}
 				std::cout << std::endl;
 			}
 		}
 
-		void fillMatrixWithRandomValues() {
-			if (m_height == m_width) {
+		void fillMatrixWithRandomValues() 
+		{
+			if (m_row == m_col) {
 				std::mt19937 rng; //TODO google this
 				srand(time(NULL));
-				for (int i = 0 ; i < m_height; ++i) {
-					for (int j = 0; j < m_height; ++j) {
+				for (int i = 0 ; i < m_row; ++i) {
+					for (int j = 0; j < m_row; ++j) {
 						m_data[i][j] = rand() % 10;
 					}
 				}
@@ -207,7 +286,7 @@ class Matrix final {
 		}
 	
 	private:
-		size_t m_width;
-		size_t m_height;
+		size_t m_col;
+		size_t m_row;
 		std::vector<std::vector<T>> m_data;
 };
