8fbbd7e9d233276debb22b1ce1a3ca0e62f3c11d
root <root@Emil-Karapetyan>
2025-01-24 00:39:19 +0400

    Added README, LICENSE, clang-tidy support as well as clang-format

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# matrix
+# Matrix Library
+
+This library provides a basic implementation of a Matrix class in C++ with functionalities such as creation, manipulation, and mathematical operations.
+
+## Features
+
+* **Matrix Creation:**
+    * Create a matrix with specified dimensions and an optional default value.
+    * Create a matrix from an initializer list.
+* **Matrix Operations:**
+    * Matrix addition and subtraction (not implemented in the provided code snippet)
+    * Matrix multiplication
+    * Matrix transposition
+* **Other Functionalities:**
+    * Get/Set row and column dimensions
+    * Access and modify individual matrix elements
+    * Calculate the determinant of a square matrix
+    * Fill the matrix with random values
+
+## Installation
+
+**Prerequisites:**
+
+* A C++ compiler (e.g., GCC, Clang)
+* CMake (https://cmake.org/)
+
+**Build:**
+
+1. **Clone the repository:**
+   ```bash
+   git clone 
+   https://github.com/EmilKarapetyan/matrix.git
+
+2. **Build project**
+   mkdir build
+   cd build
+   cmake .. 
+   make
+
+3. **Execute**
+    ./matrix_lib
--- matrix.h
+++ matrix.h
@@ -15,8 +15,8 @@ class Matrix final {
 		Matrix() :  m_row{0}, m_col{0} {};
 		Matrix(const std::size_t row, const std::size_t col, const T val=0) 
 			: m_row{row}, m_col{col}, m_data(m_row) {
-			for (std::size_t hIdx = 0; hIdx < m_row; ++hIdx) {
-				m_data[hIdx].resize(m_col, val);
+			for (std::size_t rIdx = 0; rIdx < m_row; ++rIdx) {
+				m_data[rIdx].resize(m_col, val);
 			}
 		}
 
@@ -74,9 +74,9 @@ class Matrix final {
 				return false;
 			}
 
-			for (std::uint32_t hIdx = 0; hIdx < m_row; ++hIdx) {
-				for (std::uint32_t wIdx = 0; wIdx < m_col; ++wIdx) {
-					if (m_data[hIdx][wIdx] != matr[hIdx][wIdx])
+			for (std::uint32_t rIdx = 0; rIdx < m_row; ++rIdx) {
+				for (std::uint32_t cIdx = 0; cIdx < m_col; ++cIdx) {
+					if (m_data[rIdx][cIdx] != matr[rIdx][cIdx])
 					{
 						return false;
 					}
@@ -88,19 +88,19 @@ class Matrix final {
 		{
         	return !(*this == matr);
     	}
-		inline const std::uint32_t getColumn() const
+		inline const std::size_t getColumn() const
 		{
 			return m_col;
 		}
-		inline const std::uint32_t getRow() const 
+		inline const std::size_t getRow() const 
 		{
 			return m_row;
 		}
-		inline void setcol(const std::uint32_t col) 
+		inline void setcol(const std::size_t col) 
 		{
 			m_col = col;
 		}
-		inline void setrow(const std::uint32_t row) 
+		inline void setrow(const std::size_t row) 
 		{
 			m_row = row;
 		}
@@ -108,30 +108,30 @@ class Matrix final {
 		{
 			return m_data;
 		}
-		inline void setValue(const std::uint32_t row, const std::uint32_t col, const T val) 
+		inline void setValue(const std::size_t row, const std::size_t col, const T val) 
 		{
 			m_data.at(row).at(col) = val;
 		}
-		inline T getValue(const std::uint32_t row, const std::uint32_t col) const 
+		inline T getValue(const std::size_t row, const std::size_t col) const 
 		{
 			return m_data[row][col];
 		}
-		T at(const std::uint32_t row, const std::uint32_t col) 
+		T at(const std::uint32_t row, const std::size_t col) 
 		{
 			return m_data.at(row).at(col);
 		}
-		std::vector<T>& operator[](const std::uint32_t rowIdx)  
+		std::vector<T>& operator[](const std::size_t rocIdx)  
 		{
-			return m_data.at(rowIdx);
+			return m_data.at(rocIdx);
 		}
-		std::vector<T> operator[](const std::uint32_t rowIdx) const 
+		std::vector<T> operator[](const std::size_t rocIdx) const 
 		{
-			return m_data.at(rowIdx);
+			return m_data.at(rocIdx);
 		}
 		void operator*=(T val) {
-			for (size_t hIdx = 0; hIdx < m_row; ++hIdx) {
-				for (size_t wIdx = 0; wIdx < m_col ; ++wIdx) {
-					m_data[hIdx][wIdx] *= val;
+			for (size_t rIdx = 0; rIdx < m_row; ++rIdx) {
+				for (size_t cIdx = 0; cIdx < m_col ; ++cIdx) {
+					m_data[rIdx][cIdx] *= val;
 				}
 			}
 		}
@@ -142,16 +142,16 @@ class Matrix final {
 				Matrix<T> minor(m_row-1, m_col-1);
 				std::uint32_t row = 0;
 				std::uint32_t col = 0;
-				for (std::uint32_t hIdx = 0; hIdx < m_row; ++hIdx) {
+				for (std::uint32_t rIdx = 0; rIdx < m_row; ++rIdx) {
 					col = 0;
-					if (hIdx == row) {
+					if (rIdx == row) {
 						continue;
 					}
-					for (std::uint32_t wIdx = 0; wIdx < m_col; ++wIdx) {
-						if (wIdx == col) {
+					for (std::uint32_t cIdx = 0; cIdx < m_col; ++cIdx) {
+						if (cIdx == col) {
 							continue;
 						}
-						minor.setValue(row, col++, m_data[hIdx][wIdx]);
+						minor.setValue(row, col++, m_data[rIdx][cIdx]);
 					}
 					++row;
 				}
@@ -172,8 +172,8 @@ class Matrix final {
 				Matrix<int> submatrix(m_col - 1, m_row - 1 );
 				std::uint32_t sub_i = 0;
 				for (std::size_t j = 1; j < m_row; ++j) { 
-					size_t sub_j = 0;
-					for (size_t k = 0; k < m_col; ++k) {
+					std::size_t sub_j = 0;
+					for (std::size_t k = 0; k < m_col; ++k) {
 						if (k != i) { 
 							submatrix[sub_j++][sub_i] = m_data[k][j]; 
 						}
@@ -220,9 +220,10 @@ class Matrix final {
 
 		void print()
 		{
-			for (std::uint32_t hIdx = 0; hIdx < m_row; ++hIdx) {
-				for (std::uint32_t wIdx = 0; wIdx < m_col; ++wIdx) {
-					std::cout << m_data[hIdx][wIdx] << " ";
+			for (std::size_t rIdx = 0; rIdx < m_row; ++rIdx) {
+				#pragma unroll
+				for (std::size_t cIdx = 0; cIdx < m_col; ++cIdx) {
+					std::cout << m_data[rIdx][cIdx] << " ";
 				}
 				std::cout << std::endl;
 			}
@@ -230,15 +231,21 @@ class Matrix final {
 
 		void fillMatrixWithRandomValues() 
 		{
+			const std::size_t upperLimit = 1000;
 			std::random_device rd;
     		std::mt19937 gen(rd());
-    		std::uniform_int_distribution<> dist(0, 1000);
+    		std::uniform_int_distribution<> dist(0, upperLimit);
+			
+			std::size_t rows = m_row;
+			std::size_t cols = m_col;
 
-			if (m_row == m_col) {
-				for (int i = 0 ; i < m_row; ++i) {
-					for (int j = 0; j < m_row; ++j) {
-						m_data[i][j] = dist(gen);
-					}
+			if (rows != cols) {
+				return;
+			}
+			for (std::size_t i = 0; i < rows; ++i) {
+				#pragma unroll
+				for (std::size_t j = 0; j < cols; ++j) {
+					m_data[i][j] = dist(gen);
 				}
 			}
 		}
